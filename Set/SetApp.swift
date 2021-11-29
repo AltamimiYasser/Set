f685699ccbcfbf0cda1d0df0035bb1c03d0e53c7
yasser tamimi <altamimiy89@gmail.com>
2021-11-29 05:15:08 +0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Yasser Tamimi on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
